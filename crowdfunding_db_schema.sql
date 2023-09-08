8893635d1fcec5e59cd6a3910cfb3af1e7439b91
Leo-SLC <leoantinozzi@Leos-MacBook-Pro.local>
2023-09-07 19:01:37 -0600

    Uploading PNG and Schema

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,27 +1,27 @@
 --Create a table for Contacts
 
 CREATE TABLE contacts (
-    contact_id int PRIMARY KEY NOT NULL,
+    contact_id int PK FK >- campaign.contact_id,
     first_name VARCHAR (50)  NOT NULL,
     last_name VARCHAR (50)  NOT NULL,
     email VARCHAR (100)  NOT NULL,
-	primary key (contact_id)
+        PRIMARY KEY(contact_id)
 );
 
 --Create a table for Categories
 
 CREATE TABLE category (
-    category_id VARCHAR(20) PRIMARY KEY NOT NULL,
+    category_id VARCHAR(20) FK >- campaign.category_id,
     category VARCHAR(20) NOT NULL
-    primary key (category_id)
+        PRIMARY KEY(category_id)
 );
 
 --Create a table for Subcategories
 
 CREATE TABLE subcategory (
-    subcategory_id VARCHAR(20) PRIMARY KEY NOT NULL,
+    subcategory_id VARCHAR(20) PK FK >- campaign.subcategory_id,
     subcategory VARCHAR (50) NOT NULL,
-    primary key (subcategory_id)
+        PRIMARY KEY(subcategory_id)
 );
 
 --Create a table for Campaigns
@@ -41,11 +41,16 @@ CREATE TABLE campaign (
     end_date DATE NOT NULL,
     category_id VARCHAR(20) NOT NULL,
     subcategory_id VARCHAR(20) NOT NULL
+        PRIMARY KEY(cf_id)
     
     FOREIGN KEY(contact_id) REFERENCES contacts(contact_id)
-    FOREIGN KEY(category_id) REFERENCES category (category_id)
+    FOREIGN KEY(category_id) REFERENCES category(category_id)
     FOREIGN KEY(subcategory_id) REFERENCES subcategory(subcategory_id)
 );
 
+select * from contacts
+select * from category
+select * from subcategory
+select * from campaign
 
 --cf_id,contact_id,company_name,description,goal,pledged,outcome,backers_count,country,currency,launched_date,end_date,category_id,subcategory_id
